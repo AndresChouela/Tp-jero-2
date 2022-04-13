13c67ae14ac4e0e926acb06aef739aa05a2d5f17
AndresChouela <101569013+AndresChouela@users.noreply.github.com>
2022-04-12 22:48:36 -0300

    Initial commit

---
+++ Assets/Scenes/ValidarContrasenia.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour
+{
+    string contraseniacorrecta;
+    string contraseniausuario;
+    public Text ingresousuario;
+    public Text textomsj;
+    public GameObject cartel;
+    // Use this for initialization
+    void Start()
+    {
+        contraseniacorrecta = "12345";
+        cartel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void validarcontrasenia()
+    {
+        contraseniausuario = ingresousuario.text;
+        if (contraseniacorrecta == contraseniausuario)
+        {
+            cartel.SetActive(true);
+            textomsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartel.SetActive(true);
+            textomsj.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
+
